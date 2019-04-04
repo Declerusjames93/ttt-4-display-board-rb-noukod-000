503c28395bfc61cc1cd8e165dea2aebbab1a9b86
James Declerus <james.declerus@esih.edu>
2019-04-04 15:01:09 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,8 +1,10 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+board = ["O", " ", " ", " ", " ", " ", " ", " ", " "]
+# Leave hint for assigning the 0 index value of O
 
 def display_board(board)
-
+board[0] = "O"
   puts "#{board[0]} |  #{board[1]}  |  #{board[2]}"
   puts "-----------"
   puts "#{board[3]} | #{board[4]}| #{board[5]}"
